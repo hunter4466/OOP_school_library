42b5315eb9a4b26d4288deb20db8fac7ec2180f1
Mario Chois <mario.chois.navarro@gmail.com>
2021-10-12 19:37:12 -0500

    Testing: Teachers class correctly tested

--- specs/student_spec.rb
+++ specs/student_spec.rb
@@ -12,13 +12,13 @@ describe Student do
       expect(message3).to eq true
     end
     it "should say 'Leonardo' when we call the name method" do
-        student = Student.new(15, 'Leonardo', false)
-        message = student.name
-        message2 = student.age
-        message3 = student.parentperm
-        expect(message).to eq 'Leonardo'
-        expect(message2).to eq 15
-        expect(message3).to eq false
+      student = Student.new(15, 'Leonardo', false)
+      message = student.name
+      message2 = student.age
+      message3 = student.parentperm
+      expect(message).to eq 'Leonardo'
+      expect(message2).to eq 15
+      expect(message3).to eq false
     end
   end
 end
