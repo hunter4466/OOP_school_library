f268517b438efdf9b7e80f82b9c54cbe03263b0d
Mario Chois <mario.chois.navarro@gmail.com>
2021-10-14 15:09:31 -0500

    Testing: created fizzbuzz method test -Failure

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -16,4 +16,24 @@ describe Solver do
       expect(test_rev).to eq 'oiraM'
     end
   end
+
+  context 'When testing the fizzbuzz method' do
+    fb = Solver.new
+    it 'Should return fizz if n is divisible by 3' do
+      fb_fizz = fb.fizzbuzz(8)
+      expect(fb_fizz).to eq 'fizz'
+    end
+    it 'Should return buzz if n is divisible by 5' do
+      fb_buzz = fb.fizzbuzz(9)
+      expect(fb_buzz).to eq 'buzz'
+    end
+    it 'Should return fizzbuzz if n is divisible by 3 and 5' do
+      fb_fb = fb.fizzbuzz(25)
+      expect(fb_fb).to eq 'fizzbuzz'
+    end
+    it 'Should return the integer n as a string if not divisible by 3 or 5' do
+      fb_not = fb.fizzbuzz(7)
+      expect(fb_not).to eq '7'
+    end
+  end
 end
