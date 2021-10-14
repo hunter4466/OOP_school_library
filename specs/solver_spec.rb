3af3c1d3168d352ddb43b4069c045dcb16ca48c0
Leonardo Dias <leonardodias@ucl.br>
2021-10-14 16:45:50 -0300

    Add Solver reverse and fizzbuzz methods tests

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -26,4 +26,23 @@ describe Solver do
       expect(test_rev).to eq 'dranoeL'
     end
   end
+  context "When testing the fizzbuzz method" do
+    fb = Solver.new 
+    it 'Should return fizz if n is divisible by 3' do
+      fb_fizz = fb.fizzbuzz(9)
+      expect(fb_fizz).to eq 'fizz'
+    end
+    it 'Should return buzz if n is divisible by 5' do
+      fb_buzz = fb.fizzbuzz(10)
+      expect(fb_buzz).to eq 'buzz'
+    end
+    it 'Should return fizzbuzz if n is divisible by 3 and 5' do
+      fb_fb = fb.fizzbuzz(30)
+      expect(fb_fb).to eq 'fizzbuzz'
+    end
+    it 'Should return the integer n as a string if not divisible by 3 or 5' do
+      fb_not = fb.fizzbuzz(8)
+      expect(fb_not).to eq '8'
+    end
+  end
 end
